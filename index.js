e5117705c78eb4db425e32a65fc9003aa3ddb34c
VicThorIH <victoribarrah2.gmail.com>
2021-06-24 12:56:45 -0600

    ASD22

--- index.js
+++ index.js
@@ -10,16 +10,7 @@ conectarDB();
 
 //Habilitar cors
 app.use(cors());
-
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
-   
-app.listen(80, function () {
-console.log('CORS-enabled web server listening on port 80')
-})
-
-
+ 
 //Habilitar Express.json
 app.use( express.json({extended : true}) );
 
